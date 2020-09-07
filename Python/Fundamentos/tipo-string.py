c44f4d34a50044bc1e16d0544fd6b6aa97be9f4f
Carlos Eduardo Ferreyra <48495214+Coolwinter18@users.noreply.github.com>
2020-09-07 14:01:47 -0300

    Tipo-Bool

---
+++ Python/Fundamentos/tipo-string.py
@@ -0,0 +1,10 @@
+cadena = "aerosmith"
+print("Mi grupo favorito es: " + cadena)
+numero1 = "1"
+numero2= "2"
+print("Concatenacion: " +numero1 + numero2)
+
+num1 = 1
+num2 = 2
+#print("Operacion Suma:" + str(num1 + num2))
+print("Operacion Suma:",num1 + num2)
