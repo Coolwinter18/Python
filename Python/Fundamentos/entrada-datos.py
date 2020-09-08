19be21dbf4dd1407a205281cd328dc482c0aaf2a
Carlos Eduardo Ferreyra <48495214+Coolwinter18@users.noreply.github.com>
2020-09-08 08:37:05 -0300

    fin entrada-datos.py

---
+++ Python/Fundamentos/entrada-datos.py
@@ -0,0 +1,17 @@
+#Entrada de datos en Python
+resultado = input()
+print(resultado)
+
+num1 = int(input())
+print(num1)
+num2 = int(input())
+print(num1 + num2)
+
+numero1 = input()
+numero2 = input()
+print( numero1 + numero2)
+
+#Entrada de datos en vscode
+
+resultado = input("Proporciona un valor:")
+print("el resultado es:",resultado)
