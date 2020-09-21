40f0987be85a655a907d1ac43313c1042ddeb4f8
Carlos Eduardo Ferreyra <48495214+Coolwinter18@users.noreply.github.com>
2020-09-21 08:44:09 -0300

    mundo pc done

---
+++ Python/mundo_pc/orden.py
@@ -0,0 +1,25 @@
+class Orden:
+    
+    contador_ordenes = 0
+    
+    def __init__(self, computadoras):
+        Orden.contador_ordenes += 1
+        self.__id_orden = Orden.contador_ordenes
+        #Recibe la lista de objetos de tipo Computadora
+        self.__computadoras = computadoras        
+
+    def agregar_computadora(self, computadora):
+        self.__computadoras.append(computadora) 
+    
+    def __str__(self):
+        computadoras_str = ""
+        for computadora in self.__computadoras:
+            computadoras_str += computadora.__str__()
+        
+        return (
+            f"Orden: {self.__id_orden}, " 
+            f"computadoras: {computadoras_str}"
+            )
+    def get_id_orden(self):
+        return self.__id_orden
+    # no se debe definir un set al ID de computadora ya que eso una variable autoincremental desde la variable de clase.
