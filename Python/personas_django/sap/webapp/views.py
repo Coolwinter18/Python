3ff94601838de9f9e03e8e73c939b612677b214c
Carlos Eduardo Ferreyra <48495214+Coolwinter18@users.noreply.github.com>
2020-09-28 05:10:16 -0300

    order by django done

--- Python/personas_django/sap/webapp/views.py
+++ Python/personas_django/sap/webapp/views.py
@@ -8,5 +8,6 @@ from personas.models import Persona
 def bienvenido(request):
     # return HttpResponse('Hola Mundo desde Django')
     no_personas = Persona.objects.count
-    personas = Persona.objects.all()
+    # personas = Persona.objects.all()
+    personas = Persona.objects.order_by('-id')  # +id(default sin signo +) ASC -id DESC || ponga mas de 1 parametro y ordena por primer param, y subordena por 2do param
     return render(request, 'bienvenido.html', {'no_personas': no_personas, 'personas': personas})
