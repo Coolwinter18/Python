45d3477e3e73e12476ef119de053ed3f11b86aa5
Carlos Eduardo Ferreyra <48495214+Coolwinter18@users.noreply.github.com>
2020-10-09 10:20:38 -0300

    templates en flask done

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, url_for
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
@@ -13,6 +14,19 @@ def inicio():
 def saludar(nombre):
     return f'Saludos {nombre.upper()}'
 
+
 @app.route('/edad/<int:edad>')
 def mostrar_edad(edad):
     return f'tu edad es: {edad}'
+
+
+@app.route('/mostrar/<nombre>', methods=['GET', 'POST'])
+def mostrar_nombre(nombre):
+    # return f'tu nombre es {nombre}'
+    return render_template('mostrar.html', nombre=nombre)
+
+
+@app.route('/redireccionar')
+def redireccionar():
+    # return redirect(url_for('inicio'))
+    return redirect(url_for('mostrar_nombre', nombre='Juan'))
